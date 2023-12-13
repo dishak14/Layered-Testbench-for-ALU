5326e10448562427bca031effb4864ef23e5014d
Disha Kiran <92496153+dishak14@users.noreply.github.com>
2023-12-13 10:43:08 +0530

    driver class to drive the stimulus into the DUT

---
+++ driver.sv
@@ -0,0 +1,20 @@
+class driver;
+  virtual intf vif;
+  mailbox gen2driv;
+  function new(virtual intf vif, mailbox gen2driv);
+    this.vif=vif;
+    this.gen2driv=gen2driv;
+  endfunction
+  task main;
+    repeat(1)
+      begin
+        transaction trans;
+        gen2driv.get(trans);
+        vif.a <=trans.a;
+        vif.b <=trans.b;
+        vif.s <=trans.s;
+        trans.y = vif.y;
+        trans.display("Driver");
+      end
+  endtask
+endclass
